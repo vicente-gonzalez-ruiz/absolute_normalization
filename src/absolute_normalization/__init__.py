ea0093eb927921c4d87632b8fb7085e2ae7d367c
Vicente González-Ruiz <vruiz@ual.es>
2023-10-13 14:32:33 +0200

    .

--- src/absolute_normalization/__init__.py
+++ src/absolute_normalization/__init__.py
@@ -3,6 +3,6 @@ import numpy as np
 def uint8(img, max=255, bit_depth=11):
     max_val = 1 << bit_depth
     normalized_img = (max*(img/max_val)).astype(np.uint8)
-    info = f"absolute_normalization {max_val} # max_val"
+    info = f"absolute_normalization\t {max_val} # max_val"
     return normalized_img, info
 
